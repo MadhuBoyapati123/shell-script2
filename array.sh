f94308592d5ef4ccde692ffa75e2a1e5204765e8
Madhu Boyapati <boyapatimadhubabu61118@gmail.com>
2025-04-18 20:47:03 +0530

    Array file added

--- array.sh
+++ array.sh
@@ -2,6 +2,6 @@ $!bin/bash
 
 persons=("madhu" "sai" "kiran" "suresh")
 
-echo "Enter First persons ${persons[0]}"
+echo "Enter First person ${persons[0]}"
 echo "Enter Second person ${persons[1]}"
 echo "Enter All persons ${persons[@]}"
