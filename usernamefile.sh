f69bfec21a71315b4267851427dd695f0be0005e
Madhu Boyapati <boyapatimadhubabu61118@gmail.com>
2025-04-18 20:05:22 +0530

    password added

--- usernamefile.sh
+++ usernamefile.sh
@@ -1,7 +1,11 @@
 $!bin/bash
 
 echo "please enter username"
-
 read username
-
 echo "Enter Usernamee is $username"
+
+
+echo "Please Enter password"
+read password
+echo "Entered password is $password"
+
