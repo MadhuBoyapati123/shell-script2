fe9116b7d282f53b1faf5fd6c4f5c69a58ea4543
Madhu Boyapati <boyapatimadhubabu61118@gmail.com>
2025-04-18 12:49:47 +0530

    changes made

--- variable.sh
+++ variable.sh
@@ -1,7 +1,9 @@
 #!bin/bash
 
-echo "ram: Hi raji, good morning"
-echo "raji: Good morning, How r u"
+
+
+echo "ram: Hi raju, good morning"
+echo "raju: Good morning, How r u"
 echo "ram: I am good, what about you"
-echo "raji: yeah, I am fine"
+echo "raju: yeah, I am fine"
 
