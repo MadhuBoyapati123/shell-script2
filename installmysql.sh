a992d1ede7b061bc8a1ed7dc474e034097347633
Madhu Boyapati <boyapatimadhubabu61118@gmail.com>
2025-04-19 00:22:32 +0530

    conditions added

--- installmysql.sh
+++ installmysql.sh
@@ -7,11 +7,9 @@ then
     echo "ERROR: Please run this with Root acces"
 fi
 
-
-
 yum remove postfix -y
 
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]  # Added space after 'if'
 then
     echo "Postfix installation failed"
 else
@@ -20,11 +18,9 @@ fi
 
 yum remove maven -y
 
-if[ $? -ne 0 ]
+if [ $? -ne 0 ]  # Added space after 'if'
 then
     echo "Maven installation failed"
 else
     echo "Maven installated successfully"
 fi
-
-
