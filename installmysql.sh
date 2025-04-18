a258ad3387d9527d2bbf486993dd61513dbea281
Madhu Boyapati <boyapatimadhubabu61118@gmail.com>
2025-04-19 00:10:45 +0530

    conditions added

--- installmysql.sh
+++ installmysql.sh
@@ -7,6 +7,32 @@ then
     echo "ERROR: Please run this with Root acces"
 fi
 
+
 yum install mysql-server -y
+
+if[ $? -ne 0 ]
+then
+    echo "MySQL server installation failed"
+else
+    echo "MySQL server installated successfully"
+fi
+
+yum install postfix -y
+
+if[ $? -ne 0 ]
+then
+    echo "Postfix installation failed"
+else
+    echo "Postfix installated successfully"
+fi
+
+yum install maven -y
+
+if[ $? -ne 0 ]
+then
+    echo "Maven installation failed"
+else
+    echo "Maven installated successfully"
+fi
 
 
