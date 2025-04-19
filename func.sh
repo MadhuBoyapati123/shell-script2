5aa9905285b39ae26dd920d6d97bfe81717a6b3c
Madhu Boyapati <boyapatimadhubabu61118@gmail.com>
2025-04-19 14:09:05 +0530

    Functions added

--- func.sh
+++ func.sh
@@ -1,30 +1,28 @@
-#!bin/bash
+#!/bin/bash
 
+USERID=$(id -u)
 
-USERID=$(id-u)
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo "Installation Failure please check the logs"
+        exit 1
+    else
+        echo "Installation Success You have done great work"
+    fi
+}
 
-VALIDATE()
-
-if [ $1 -ne 0 ]
-then
-    echo "Installation Failure please check the logs"
-    exit 1
-else
-    echo "Installation Success You have done great work"
-fi
-
-
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
     echo "ERROR: Please run this with Root access"
     exit 1
 fi
 
 yum install postfix -y
-VALIDATE($?)
-yum install maven -ty
-VALIDATE($?)
+VALIDATE $?
 
+yum install maven -y
+VALIDATE $?
 
 
 
